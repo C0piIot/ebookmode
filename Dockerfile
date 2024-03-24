2868db9698f88dde3c69861fc2b5895d83fff6ed
Eduard Martinez <martinez.bernal@gmail.com>
2024-03-24 21:53:53 +0000

    docker de prod y dev

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,21 @@
-FROM node:current-alpine
-RUN mkdir -p /app/node_modules && chown -R node:node /app
-WORKDIR /app
-COPY package*.json ./
-USER node
-RUN npm install
-COPY --chown=node:node . .
+FROM node:current-alpine AS base
+RUN mkdir -p /app
+EXPOSE 80
 
-EXPOSE 8080
+FROM base AS development
+ENV NODE_ENV=development
+COPY package*.json /
+RUN npm install
+WORKDIR /app
+CMD [ "node", "node_modules/nodemon/bin/nodemon.js" ]
+
+FROM base AS production
+ENV NODE_ENV=production
+RUN chown node:node /app
+RUN mkdir -p /app/node_modules
+RUN chown node:node /app/node_modules
+USER node
+WORKDIR /app
+COPY --chown=node:node . .
+RUN npm install
 CMD [ "node", "app.js" ]
--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import { JSDOM } from 'jsdom';
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("hello");
-});
-
-app.listen(8080);
+app.get("/", (request, response) => {
+    const url = request.query?.url;
+    if(url) {
+        const doc = new JSDOM("<body>Look at this cat: <img src='./cat.jpg'></body>", {
+            url: "https://www.example.com/the-page-i-got-the-source-from"
+          });
+        const reader = new Readability(doc.window.document);
+        const article = reader.parse();
+        response.send(reader.content);
+    } else {
+        response.send("url is missing");
+    }
+});  
+app.listen(80);
